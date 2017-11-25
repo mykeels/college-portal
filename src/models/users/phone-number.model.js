1b91a9d380d58b74a84f03caaa8a22e759aa3ded
Ikechi Michael <mykehell123@gmail.com>
2017-11-25 16:34:40 +0100

    feat: phone-number.model returns a factory function

--- src/models/users/phone-number.model.js
+++ src/models/users/phone-number.model.js
@@ -1,24 +1,26 @@
 import Sequelize from 'sequelize'
 
-const PhoneNumber = Sequelize.define('phone_number', {
-    id: {
-        type: Sequelize.NUMBER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    phone: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    creation_date: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
-        allowNull: false
-    },
-    active: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
-    }
-})
-
-export default PhoneNumber
+export default (sequelize) => {
+    const PhoneNumber = sequelize.define('phone_number', {
+        id: {
+            type: Sequelize.NUMBER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        phone: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        creation_date: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW,
+            allowNull: false
+        },
+        active: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        }
+    })
+    
+    return PhoneNumber
+}
