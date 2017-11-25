9288c73df1d3e0eeb5eeb9711c217fc18795746f
Ikechi Michael <mykehell123@gmail.com>
2017-11-25 17:12:36 +0100

    chore: remove sails-disk from index.config

--- src/index.config.js
+++ src/index.config.js
@@ -1,15 +1,3 @@
-import SailsDisk from 'sails-disk'
-
-//SailsDisk.registerDatastore = SailsDisk.registerConnection
-//SailsDisk.registerConnection = undefined
-
 export default {
-    adapters: {
-        disk: SailsDisk
-    },
-    datastores: {
-        default: {
-            adapter: 'disk'
-        }
-    }
+
 }
