940bb3d813bfdfc9591dfa80cbe69b2a128ed8fd
Ikechi Michael <mykehell123@gmail.com>
2017-11-25 19:17:37 +0100

    feat: make connection details agnostic

--- src/db/index.js
+++ src/db/index.js
@@ -6,13 +6,14 @@ console.log(process.env.DB_HOST)
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_TYPE,
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
     },
+    storage: process.env.DB_PATH,
     operatorsAliases: false
   });
 
